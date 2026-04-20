b779bd9bac537cce3a6ff010a19abaaf2f064afd
purarecoveryryan <purarecoveryryan@gmail.com>
2026-04-20 12:50:52 +0000

    thyg

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -6,11 +6,9 @@
 
   # Use https://search.nixos.org/packages to find packages
   packages = [
-    # pkgs.go
-    # pkgs.python311
-    # pkgs.python311Packages.pip
-    # pkgs.nodejs_20
-    # pkgs.nodePackages.nodemon
+    pkgs.python311
+    pkgs.python311Packages.pip
+    pkgs.python311Packages.uvicorn
   ];
 
   # Sets environment variables in the workspace
@@ -25,16 +23,11 @@
     previews = {
       enable = true;
       previews = {
-        # web = {
-        #   # Example: run "npm run dev" with PORT set to IDX's defined port for previews,
-        #   # and show it in IDX's web preview panel
-        #   command = ["npm" "run" "dev"];
-        #   manager = "web";
-        #   env = {
-        #     # Environment variables to set for your server
-        #     PORT = "$PORT";
-        #   };
-        # };
+        rhythmforge = {
+          command = ["uvicorn" "app:app" "--host" "0.0.0.0" "--port" "$PORT"];
+          manager = "web";
+          label = "RhythmForge";
+        };
       };
     };
 
@@ -42,8 +35,7 @@
     workspace = {
       # Runs when a workspace is first created
       onCreate = {
-        # Example: install JS dependencies from NPM
-        # npm-install = "npm install";
+        install-deps = "pip install -r requirements.txt";
       };
       # Runs when the workspace is (re)started
       onStart = {
